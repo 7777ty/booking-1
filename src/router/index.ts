291dc496303ca3b0deae1c4ad8e5f814d3ea1517
7777ty <568318359@qq.com>
2020-01-18 22:31:56 +0800

    404页面添加

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
@@ -23,6 +24,10 @@ const routes = [
   {
     path: '/statistics',
     component: Statistics
+  },
+  {
+    path: '*',
+    component: NotFound
   }
 ];
 
