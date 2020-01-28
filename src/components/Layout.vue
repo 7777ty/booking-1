49ba4c566dc1b4a5ff52357a399f8dd3a0776a2a
7777ty <568318359@qq.com>
2020-01-28 20:32:16 +0800

    导航完成

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="nav-wrapper">
     <div class="money">
-        <slot/>
+      <slot />
     </div>
     <Nav />
   </div>
@@ -15,13 +15,11 @@ export default {
 
 <style lang="scss" scoped>
 .nav-wrapper {
-  border: 1px solid green;
   display: flex;
   flex-direction: column;
   height: 100vh;
 }
 .money {
-  border: 1px solid blue;
   overflow: auto;
   flex-grow: 1;
 }
